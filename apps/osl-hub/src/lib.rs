67a217ec539187df505dca8ca1596f584c5b8920
OSLPrivacy <283250147+OSLPrivacy@users.noreply.github.com>
2026-08-01 21:20:26 -0700

    t15-f4: bind USB dead-man actions

--- apps/osl-hub/src/lib.rs
+++ apps/osl-hub/src/lib.rs
@@ -128,6 +128,8 @@ pub mod discord_qa_inbound_receipt;
 #[cfg(feature = "core")]
 pub mod destruct_ack_rollup;
 #[cfg(feature = "core")]
+pub mod deadman;
+#[cfg(feature = "core")]
 pub mod device_transfer;
 #[cfg(feature = "core")]
 pub mod identity_binding_verifier;
