fbd6a7816167df3cdb053bbf4fe9c82cfe293092
OSLPrivacy <283250147+OSLPrivacy@users.noreply.github.com>
2026-08-08 14:34:01 -0700

    TASK 3222 contain dangerous attachment names

--- apps/osl-hub/src/lib.rs
+++ apps/osl-hub/src/lib.rs
@@ -213,6 +213,8 @@ pub mod native_window_host;
 #[cfg(feature = "core")]
 pub mod osl_chat_attachment_download_permission;
 #[cfg(feature = "core")]
+pub mod osl_chat_attachment_filename;
+#[cfg(feature = "core")]
 pub mod osl_chat_content_name;
 #[cfg(feature = "core")]
 pub mod osl_chat_drag_drop;
